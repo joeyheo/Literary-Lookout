6656686ac778e82826e5e4235562387a7ad3391a
Joey Heo <73047475+joeyheo@users.noreply.github.com>
2023-07-12 10:06:31 +0900

    Update firebaseConfig.tsx

--- src/firebase/firebaseConfig.tsx
+++ src/firebase/firebaseConfig.tsx
@@ -10,13 +10,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCLpDR8KQiAkWqRunFoImx0C7MhEfJGRM0",
-  authDomain: "literarylookout.firebaseapp.com",
-  projectId: "literarylookout",
-  storageBucket: "literarylookout.appspot.com",
-  messagingSenderId: "403702741437",
-  appId: "1:403702741437:web:029ac242efc061010dfad4",
-  measurementId: "G-KNF2RHG510",
+  apiKey: "_",
+  authDomain: "_",
+  projectId: "_",
+  storageBucket: "_",
+  messagingSenderId: "_",
+  appId: "_",
+  measurementId: "_",
 };
 
 // Initialize Firebase
